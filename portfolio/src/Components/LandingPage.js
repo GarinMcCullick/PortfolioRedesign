705cc26da26671f6aab695a0a753d980715a91a8
Garin Mccullick <glmccullick@gmail.com>
2022-11-29 17:27:06 -0600

    changed landing page

--- portfolio/src/Components/LandingPage.js
+++ portfolio/src/Components/LandingPage.js
@@ -19,26 +19,27 @@ const InnerContainer = styled.div`
 const About = styled.div`
   width: 60%;
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Title = styled.div`
   margin-top: 10%;
   width: 100%;
-  height: 10%;
+  height: 20%;
   text-align: center;
   font-size: 48px;
+  border: 5px double crimson;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
 `;
 
 const Span = styled.span`
   color: crimson;
   border-bottom: 2px solid crimson;
-`;
-
-const Body = styled.div`
-  width: 90%;
-  height: auto;
-  font-size: 24px;
-  line-height: 2.5rem;
 `;
 
 const PictureContainer = styled.div`
@@ -68,23 +69,9 @@ export default function LandingPage() {
               Hi My name is <Span>Garin McCullick</Span>
             </h1>
           </Title>
-          <Body>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum
-            </p>
-          </Body>
         </About>
         <PictureContainer>
-          <Pic src={process.env.PUBLIC_URL + "/profile2.jpg"}></Pic>
+          <Pic src={process.env.PUBLIC_URL + "/profile6.jpeg"}></Pic>
         </PictureContainer>
       </InnerContainer>
     </Container>
