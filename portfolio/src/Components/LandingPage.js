e7983963da79b6a381074c2ec17076783fb186a2
Garin McCullick <glmccullick@gmail.com>
2024-06-26 22:13:22 -0500

    updated the landing and made it responsive

--- portfolio/src/Components/LandingPage.js
+++ portfolio/src/Components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { IoTrendingDownOutline } from "react-icons/io5";
 
 const Container = styled.section`
@@ -9,12 +9,16 @@ const Container = styled.section`
   justify-content: center;
   align-items: top;
 `;
+
 const InnerContainer = styled.div`
   margin-top: 3%;
   height: 80%;
   width: 85%;
   display: flex;
-  flex-flow: column wrap;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
   @media (max-width: 1000px) {
     width: 100%;
     height: 100%;
@@ -27,23 +31,28 @@ const About = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
   @media (max-width: 1000px) {
     width: 100%;
     height: 100%;
+    order: 2;
   }
 `;
 
 const Title = styled.div`
   padding: 5%;
   width: 100%;
-  height: 20%;
+  height: auto;
   text-align: center;
-  font-size: 48px;
-  border-bottom: 5px solid crimson;
-  border-top: 5px solid crimson;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 32px;
+  line-height: 3rem;
 `;
 
 const Span = styled.span`
@@ -57,6 +66,7 @@ const PictureContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
   @media (max-width: 1000px) {
     display: none;
   }
@@ -69,9 +79,9 @@ const Pic = styled.img`
   background-repeat: no-repeat;
   border-radius: 10px;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+
   @media (max-width: 1000px) {
-    height: 80%;
-    width: 80%;
+    display: none;
   }
 `;
 
@@ -84,13 +94,25 @@ const Div = styled.div`
 const Span2 = styled.div`
   display: flex;
   flex-flow: column wrap;
+  align-items: center;
+  animation: ${keyframes`
+    0%, 20%, 50%, 80%, 100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-20px);
+    }
+    60% {
+      transform: translateY(-10px);
+    }
+  `} 4s ease-in-out infinite;
 `;
 
 const DoubleDown = styled(IoTrendingDownOutline)`
   margin-top: -4px;
   font-size: 70px;
   color: crimson;
-  transform: rotate(45deg);
+  transform: rotate(45deg); /* Rotate arrow to point downwards */
 `;
 
 export default function LandingPage() {
@@ -100,11 +122,12 @@ export default function LandingPage() {
         <About>
           <Div>
             <Title>
-              <h1>
-                My name is <Span>Garin McCullick</Span>.<br />
+              <H1>
+                My name is <br />
+                <Span>Garin McCullick</Span>
                 <br />
                 Welcome to my portfolio!
-              </h1>
+              </H1>
             </Title>
             <Span2>
               <DoubleDown />
